b05a844503d5939324892166787f3174a40b9595
Omar Ragab <66734293+omarragab2020@users.noreply.github.com>
2026-09-16 07:03:01 +0300

    Restore: chromium CanvasKit bootstrap + v4 favicon cache bust for updated profile photo

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,16 @@
+{{flutter_js}}
+
+{{flutter_build_config}}
+
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: {{flutter_service_worker_version}},
+  },
+  onEntrypointLoaded: async function(engineInitializer) {
+    let appRunner = await engineInitializer.initializeEngine({
+      renderer: "canvaskit",
+      canvasKitVariant: "chromium",
+    });
+    await appRunner.runApp();
+  }
+});
